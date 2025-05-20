c13c4eeb2aa4c329d5baf3451a08d3e0e3eda5ce
Kelvyn Paiva <kelvynmpaiva@gmail.com>
2025-05-19 22:07:08 -0300

    criando a funcao criar curso.

--- Admin.js
+++ Admin.js
@@ -4,9 +4,13 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = "admin", ativo = true){
         super(nome, email, nascimento, role, ativo)
     }
+
+    criarCurso(nomeCurso, qtdVagas){
+        return `curso ${nomeCurso} foi criado com ${qtdVagas} vagas.`
+    }
 }
 
 const novoAdmin = new Admin('Pedro', 'pedro@gmail.com', '1999-08-01')
 
 console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('Java', 20));
